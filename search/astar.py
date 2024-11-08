f1d984b39e69b972fd17772145d159d334e6a1dc
patinoboh <patrik.brocek@gmail.com>
2024-11-08 15:54:46 +0100

    too much memory

--- search/astar.py
+++ search/astar.py
@@ -7,40 +7,28 @@ def AStar(prob: HeuristicProblem) -> Solution:
     """Return Solution of the problem solved by AStar search."""
     # Your implementation goes here.
 
-    # VRAJ STAČÍ NAKOPČIŤ UCS A PRIDAŤ HEURISTIKU
-    # h_value = prob.estimate(state)
-
-    # POZOR že keď používame tuples a chceme ich radiť tak niekedy python nemusí vedieť porovnávač
-    # 
-
-    # sekunda je vteřina
-    # ale milisekunda je už milisekunda
     Q = PriorityQueue()
     count = 0
     heuristic = prob.estimate(prob.initial_state())
-    # real + heuristic, real, count, state, actions
+
+    #      f + h,     f, count, state,              actions
     Q.put((heuristic, 0, count, prob.initial_state(), []))
     visited = set()
 
     while not Q.empty():
-        _, real_cost, _, state, actions = Q.get()
+        _, path_cost, _, state, actions = Q.get()
 
         if prob.is_goal(state):
-            return Solution(actions, state, real_cost)
+            return Solution(actions, state, path_cost)
         
         if state not in visited:
             visited.add(state)
             for a in prob.actions(state):
                 new_state = prob.result(state, a)
+                action_cost = prob.cost(state, a)
                 if new_state not in visited:
                     count += 1
-                    cost = prob.cost(state, a)
                     heuristic = prob.estimate(new_state)
-                    Q.put( (real_cost + cost + heuristic, real_cost + cost, count, new_state, actions + [a] ) )
+                    Q.put( (path_cost + action_cost + heuristic, path_cost + action_cost, count, new_state, actions + [a] ) )
     return None
 
-
-
-    raise NotImplementedError
-
-    # return Solution([actions leading to goal], goal_state, path_cost)
