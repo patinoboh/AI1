c72b54432aae5c39b3c9b155a43a6b578516f59a
patinoboh <patrik.brocek@gmail.com>
2024-11-08 13:34:31 +0100

    work in progress

--- search/ucs.py
+++ search/ucs.py
@@ -5,7 +5,24 @@ from typing import Optional
 
 def ucs(prob: Problem) -> Optional[Solution]:
     """Return Solution of the problem solved by UCS search."""
-    # Your implementation goes here.
-    raise NotImplementedError
+    # ucs = UniformCostSearch
+    
+    # graph search = no repeated state
+    # tree search = repeated state
+    
+    Q = [(prob.initial_state(), [])]
+    visited = [prob.initial_state()]
+    actions = []
 
+    while Q:
+        state, actions = Q.pop(0)
+        for a in prob.actions(state):
+            new_state = prob.result(state, a)            
+            if(prob.is_goal(new_state)):
+                return Solution(actions.append(a), new_state,len(actions) + 1)
+            
+            if new_state not in visited:
+                Q.append((new_state, actions + [a]))
+                visited.append(new_state)
+    return None
     # return Solution([actions leading to goal], goal_state, path_cost)
