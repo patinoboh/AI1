d140a19f49807a14d3b7bffbc42d2163ccb3c1dc
patinoboh <patrik.brocek@gmail.com>
2024-11-08 18:41:50 +0100

    retrieve path via dictionary

--- search/astar.py
+++ search/astar.py
@@ -3,6 +3,14 @@ from search_templates import Solution, HeuristicProblem
 from queue import PriorityQueue
 
 
+def retrieve_actions(actions, state):
+    result = []
+    action = None
+    while state is not None and state in actions:
+        state, action = actions[state]
+        result.append(action)
+    return result[:-1][::-1]
+
 def AStar(prob: HeuristicProblem) -> Solution:
     """Return Solution of the problem solved by AStar search."""
     # Your implementation goes here.
@@ -11,24 +19,25 @@ def AStar(prob: HeuristicProblem) -> Solution:
     count = 0
     heuristic = prob.estimate(prob.initial_state())
 
-    #      f + h,     f, count, state,              actions
-    Q.put((heuristic, 0, count, prob.initial_state(), []))
-    visited = set()
+    #      g + h,     g, count, state,     previouse_state, action
+    Q.put((heuristic, 0, count, prob.initial_state(), None, None))
+    visited = {}
 
     while not Q.empty():
-        _, path_cost, _, state, actions = Q.get()
+        _, path_cost, _, state, old_state, action = Q.get()
 
         if prob.is_goal(state):
-            return Solution(actions, state, path_cost)
+            visited[state] = (old_state, action)
+            return Solution(retrieve_actions(visited, state), state, path_cost)
         
         if state not in visited:
-            visited.add(state)
+            visited[state] = (old_state, action)
             for a in prob.actions(state):
                 new_state = prob.result(state, a)
                 action_cost = prob.cost(state, a)
                 if new_state not in visited:
                     count += 1
                     heuristic = prob.estimate(new_state)
-                    Q.put( (path_cost + action_cost + heuristic, path_cost + action_cost, count, new_state, actions + [a] ) )
+                    Q.put( (path_cost + action_cost + heuristic, path_cost + action_cost, count, new_state, state, a ) )
     return None
 
--- search/ucs.py
+++ search/ucs.py
@@ -4,6 +4,13 @@ from typing import Optional
 
 from queue import PriorityQueue
 
+def retrieve_actions(actions, state):
+    result = []
+    action = None
+    while state is not None and state in actions:
+        state, action = actions[state]
+        result.append(action)
+    return result[:-1][::-1]
 
 def ucs(prob: Problem) -> Optional[Solution]:
     """Return Solution of the problem solved by UCS search."""
@@ -14,22 +21,24 @@ def ucs(prob: Problem) -> Optional[Solution]:
     
     Q = PriorityQueue()
     count = 0
-    Q.put((0, count, prob.initial_state(), []))
-    visited = set()
+    #   path, count,    state,    previous_state,  action
+    Q.put((0, count, prob.initial_state(), None, None))
+    
+    visited = {}
 
     while not Q.empty():
-        path_cost, _, state, actions = Q.get()
+        path_cost, _, state, old_state, action = Q.get()
 
         if prob.is_goal(state):
-            return Solution(actions, state, path_cost)
-        
+            visited[state] = (old_state, action)
+            return Solution(retrieve_actions(visited, state), state, path_cost)
+
         if state not in visited:
-            visited.add(state)
+            visited[state] = (old_state, action)
             for a in prob.actions(state):
                 new_state = prob.result(state, a)
                 action_cost = prob.cost(state, a)
                 if new_state not in visited:
                     count += 1
-                    Q.put( (path_cost + action_cost, count, new_state, actions + [a] ) )
+                    Q.put( (path_cost + action_cost, count, new_state, state, a ) )
     return None
-    # return Solution([actions leading to goal], goal_state, path_cost)
