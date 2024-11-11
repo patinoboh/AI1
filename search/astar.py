0da086b6aa5012c9fc4bd5e501188922f757d341
patinoboh <patrik.brocek@gmail.com>
2024-11-11 12:01:47 +0100

    using heapq

--- search/astar.py
+++ search/astar.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
+from dataclasses import dataclass
 from search_templates import Solution, HeuristicProblem
-from queue import PriorityQueue
+import heapq as hq
+
+
+@dataclass
+class Node:
+    heuristic_cost : float
+    path_cost : float
+    count : int
+    state : object
+    old_state : object
+    action : object
+    def __lt__(self, other):
+        return (self.heuristic_cost, self.path_cost, self.count) < (other.heuristic_cost, other.path_cost, other.count)
 
 
 def retrieve_actions(actions, state):
@@ -15,21 +28,24 @@ def AStar(prob: HeuristicProblem) -> Solution:
     """Return Solution of the problem solved by AStar search."""
     # Your implementation goes here.
 
-    Q = PriorityQueue()
     count = 0
     heuristic = prob.estimate(prob.initial_state())
+    q = [Node(heuristic, 0, count, prob.initial_state(), None, None)]
 
-    #      g + h,     g, count, state,     previouse_state, action
-    Q.put((heuristic, 0, count, prob.initial_state(), None, None))
     visited = {}
 
-    while not Q.empty():
-        _, path_cost, _, state, old_state, action = Q.get()
+    while q:
+        node = hq.heappop(q)
+
+        path_cost = node.path_cost        
+        state = node.state
+        old_state = node.old_state
+        action = node.action
 
         if prob.is_goal(state):
             visited[state] = (old_state, action)
             return Solution(retrieve_actions(visited, state), state, path_cost)
-        
+
         if state not in visited:
             visited[state] = (old_state, action)
             for a in prob.actions(state):
@@ -38,6 +54,7 @@ def AStar(prob: HeuristicProblem) -> Solution:
                 if new_state not in visited:
                     count += 1
                     heuristic = prob.estimate(new_state)
-                    Q.put( (path_cost + action_cost + heuristic, path_cost + action_cost, count, new_state, state, a ) )
+                    new_node = Node(path_cost + action_cost + heuristic, path_cost + action_cost, count, new_state, state, a)
+                    hq.heappush(q, new_node)
     return None
 
