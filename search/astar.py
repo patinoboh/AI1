bb28b3b998e510035bec6323a09434b81da692b9
patinoboh <patrik.brocek@gmail.com>
2024-11-12 19:47:45 +0100

    STILL NOT FUCKIN GOOD ENOUGH

--- search/astar.py
+++ search/astar.py
@@ -7,57 +7,53 @@ import heapq as hq
 @dataclass
 class Node:
     heuristic_cost : float
-    path_cost : float
+    real_cost : float
     count : int
     state : object
     old_state : object
     action : object
     def __lt__(self, other):
-        return (self.heuristic_cost, self.path_cost, self.count) < (other.heuristic_cost, other.path_cost, other.count)
+        return (self.heuristic_cost, self.real_cost, self.count) < (other.heuristic_cost, other.real_cost, other.count)
 
 
 def retrieve_actions(actions, state):
+    node = None
     result = []
-    action = None
     while state is not None and state in actions:
-        state, action = actions[state]
-        result.append(action)
+        node = actions[state]
+        state = node.old_state
+        result.append(node.action)
     return result[:-1][::-1]
 
 def AStar(prob: HeuristicProblem) -> Solution:
     """Return Solution of the problem solved by AStar search."""
 
     count = 0
-    heuristic = prob.estimate(prob.initial_state())
-    q = [Node(heuristic, 0, count, prob.initial_state(), None, None)]
-    q_d = {prob.initial_state(): 0}
+    q = [Node(prob.estimate(prob.initial_state()), 0, count, prob.initial_state(), None, None)]
 
-    visited = {}
+    visited = {} # not exapanded nodes, but all nodes that have been in queue so far
 
     while q:
         node = hq.heappop(q)
-        del q_d[node.state]
 
-        path_cost = node.path_cost
         state = node.state
         old_state = node.old_state
         action = node.action
+        real_cost = node.real_cost
 
         if prob.is_goal(state):
-            visited[state] = (old_state, action)
-            return Solution(retrieve_actions(visited, state), state, path_cost)
-
+            visited[state] = node
+            return Solution(retrieve_actions(visited, state), state, real_cost)
+        
         if state not in visited:
-            visited[state] = (old_state, action)
+            visited[state] = node
             for a in prob.actions(state):
                 new_state = prob.result(state, a)
                 action_cost = prob.cost(state, a)
                 heuristic = prob.estimate(new_state)
-                if new_state not in visited:
-                    if (new_state not in q_d) or (q_d[new_state] > path_cost + action_cost + heuristic):
-                        count += 1
-                        new_node = Node(path_cost + action_cost + heuristic, path_cost + action_cost, count, new_state, state, a)
-                        hq.heappush(q, new_node)
-                        q_d[new_state] = path_cost + action_cost + heuristic
+                if (new_state not in visited) or (visited[new_state].heuristic_cost > real_cost + action_cost + heuristic):
+                    count += 1
+                    new_node = Node(real_cost + action_cost + heuristic, real_cost + action_cost, count, new_state, state, a)
+                    hq.heappush(q, new_node)
     return None
 
