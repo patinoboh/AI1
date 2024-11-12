1b8d0898639e550f19d7d76422698409aec1fc81
patinoboh <patrik.brocek@gmail.com>
2024-11-12 17:27:39 +0100

    check

--- search/sokoban/dead_square_detector.py
+++ search/sokoban/dead_square_detector.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from game.board import Board, ETile
 from typing import List
+from queue import Queue
 
 
 def detect(board: Board) -> List[List[bool]]:
@@ -21,4 +22,32 @@ def detect(board: Board) -> List[List[bool]]:
     (This structure can be indexed "struct[x][y]"
      to get value on position (x, y).)
     """
-    return [[False for _ in range(board.height)] for _ in range(board.width)]
+    def in_range(x1, y1):
+        return 0 <= x1 < board.width and 0 <= y1 < board.height
+    
+    dead_end = True
+    result = [[dead_end for _ in range(board.height)] for _ in range(board.width)]
+
+    q = Queue()
+    moves = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+    for i in range(board.width):
+        for j in range(board.height):
+            if ETile.is_target(board.tile(i, j)):
+                result[i][j] = not dead_end
+                q.put((i, j))
+
+    visited = set()
+
+    while not q.empty():
+        x,y = q.get()
+        result[x][y] = not dead_end
+        visited.add((x, y))
+
+        for dx, dy in moves:
+            nx, ny = x + dx, y + dy
+            sx, sy = x + 2 * dx, y + 2 * dy
+            if in_range(sx, sy) and not ETile.is_wall(board.tile(nx, ny)) and not ETile.is_wall(board.tile(sx, sy)) and (nx, ny) not in visited:
+                q.put((nx, ny))
+
+    return result
