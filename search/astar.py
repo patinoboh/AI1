228876a3b18cf3a03aaff968bdd12c5d30edca97
patinoboh <patrik.brocek@gmail.com>
2024-11-13 15:21:00 +0100

    tried storing path directly in the q

--- search/astar.py
+++ search/astar.py
@@ -10,8 +10,7 @@ class Node:
     real_cost : float
     count : int
     state : object
-    old_state : object
-    action : object
+    actions : list
     def __lt__(self, other):
         return (self.heuristic_cost, self.real_cost, self.count) < (other.heuristic_cost, other.real_cost, other.count)
 
@@ -29,35 +28,38 @@ def AStar(prob: HeuristicProblem) -> Solution:
     """Return Solution of the problem solved by AStar search."""
 
     count = 0 # tie breaker
-    q = [Node(0 + 0 + prob.estimate(prob.initial_state()), 0, count, prob.initial_state(), None, None)]
+    q = [Node(0 + 0 + prob.estimate(prob.initial_state()), 0, count, prob.initial_state(), [])]
 
-    visited = {} # not exapanded nodes, but all nodes that have been in queue so far
+    visited = set()
 
     while q:
         node = hq.heappop(q)
 
         state = node.state
-        real_cost = node.real_cou
+        real_cost = node.real_cost
+        
         if prob.is_goal(state):
-            visited[state] = node
-            return Solution(retrieve_actions(visited, state), state, real_cost)
+            return Solution(node.actions, state, real_cost)
         
         if state not in visited:
-            visited[state] = node
+            visited.add(state)
             for a in prob.actions(state):
                 new_state = prob.result(state, a)
                 action_cost = prob.cost(state, a)
                 heuristic = prob.estimate(new_state)
                 count += 1
                 if new_state not in visited:
-                    in_q = [n for n in q if n.state == new_state]
-                    new_node = Node(heuristic + real_cost + action_cost, real_cost + action_cost, count, new_state, state, a)
-                    if in_q:
-                        q = [n for n in q if n.state != new_state]
-                        q.append(new_node)
-                        hq.heapify(q)
-                    else:
-                        hq.heappush(q, new_node)
+                    new_node = Node(heuristic + real_cost + action_cost, real_cost + action_cost, count, new_state, node.actions + [a])
+                    hq.heappush(q, new_node)
+
+                    # in_q = [n for n in q if n.state == new_state]
+                    # new_node = Node(heuristic + real_cost + action_cost, real_cost + action_cost, count, new_state, state, a)
+                    # if in_q:
+                    #     q = [n for n in q if n.state != new_state]
+                    #     q.append(new_node)
+                    #     hq.heapify(q)
+                    # else:
+                    #     hq.heappush(q, new_node)
 
                         
 
